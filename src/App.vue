3615086dd3d24cbd3478a23467ad8f2f9b8e4fc8
qq282126990 <282126990@qq.com>
2018-12-27 17:46:33 +0800

    0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,68 @@
 <template>
       <div id="app">
-            <router-view/>
+            <!-- 头部 -->
+            <div class="app-header">
+                  <div class="app-header-wrapper">
+                        <div class="app-header-img">
+                              <img src="https://cn.vuejs.org/images/logo.png">
+                              <p class="text">IVue</p>
+                        </div>
+                  </div>
+            </div>
+            <div class="app-content">
+                  <!-- 左导航 -->
+                  <div class="app-content-left">
+                        <ul class="router-lists">
+                              <router-link
+                                    class="router-lists-item"
+                                    v-for="item in routers"
+                                    :key="item.name"
+                                    :to="item.route"
+                              >
+                                    {{item.name}}
+                                    <ul v-if="item.child.length > 0">
+                                          <li
+                                                class="router-lists-item-child"
+                                                v-for="child in item.child"
+                                                :key="child.name"
+                                          >{{child.name}}</li>
+                                    </ul>
+                              </router-link>
+                        </ul>
+                  </div>
+                  <!-- 右内容 -->
+                  <div class="app-content-right">
+                        <router-view/>
+                  </div>
+            </div>
       </div>
 </template>
+
+<script>
+
+export default {
+      name: 'app',
+      data () {
+            return {
+                  /**
+                   * 路由导航
+                   * 
+                   * @type {Array}
+                   */
+                  routers: [
+                        {
+                              name: '安装',
+                              route: '/install',
+                              child: [
+                              ]
+                        }
+                  ]
+            }
+      },
+      components: {
+      }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -16,5 +76,89 @@
       flex-direction: column;
       color: #000;
       background-color: #f5f5f5;
+}
+
+.app {
+      /*头部*/
+      &-header {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            height: 60px;
+            background-color: #fff;
+            box-shadow: 0 0 5px rgba(57, 70, 78, 0.2);
+            &-wrapper {
+                  display: flex;
+                  align-items: center;
+                  width: 95%;
+                  height: 100%;
+                  margin: 0 auto;
+            }
+            &-img {
+                  display: flex;
+                  img {
+                        height: 40px;
+                  }
+                  .text {
+                        margin-left: 20px;
+                        font-size: 26px;
+                        color: #000;
+                  }
+            }
+            z-index: 100;
+      }
+
+      /*内容*/
+      &-content {
+            position: relative;
+            margin-top: 60px;
+            height: auto;
+            zoom: 1;
+            display: block;
+            z-index: 50;
+                  background-color: #fff;
+            /*左*/
+            &-left {
+                  position: relative;
+                  display: block;
+                  padding: 24px 26px;
+                  width: 16.66666667%;
+                  float: left;
+                  flex: 0 0 auto;
+                  box-shadow: 0 0 0 rgba(0, 0, 0, 0.5);
+                  border-right: 1px solid #e4e7e9;
+
+                  .router-lists {
+                        display: flex;
+                        flex-direction: column;
+                        &-item {
+                              flex: 1;
+                              margin-bottom: 8px;
+                              text-align: left;
+                              font-size: 18px;
+                              font-weight: 600;
+                              color: #7f8c8d;
+                              ul {
+                                    padding-left: 13px;
+                              }
+                              &-child {
+                                    margin-top: 7px;
+                                    font-weight: initial;
+                                    font-size: 14px;
+                              }
+                        }
+                  }
+            }
+            /*右*/
+            &-right {
+                  position: relative;
+                  text-align: left;
+                  padding: 20px;
+                  float: left;
+                  flex: 0 0 auto;
+                  display: block;
+                  width: 83.33333333%;
+            }
+      }
 }
 </style>
